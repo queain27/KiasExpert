cfc97319d788275c79d4cb63d4944957d9717e35
Ctfarhana3012 <cfarhana30@gmail.com>
2024-07-23 12:27:55 +0800

    fix form

--- pages/SectionB/addResearch.php
+++ pages/SectionB/addResearch.php
@@ -116,7 +116,7 @@ if(isset($_POST['submit'])) {
           <!--Amount Received-->
           <div class="col-md-6 mb-3">
             <label class="form-label">AMOUNT RECEIVED:"</label>
-            <input type="text" class="form-control" name="amtrec" placeholder="Amount Received" required>
+            <input type="text" class="form-control" name="amt_rec" placeholder="Amount Received" required>
           </div>
          
           <!--Remarks-->
